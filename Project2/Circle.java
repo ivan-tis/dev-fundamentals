4c5943bd96cba02da05cb01d8ffe8213a5ee3ac0
admin <admin@SABOCPT01.jalasoft.local>
2017-06-07 09:37:11 -0400

    task 3

--- Project2/Circle.java
+++ Project2/Circle.java
@@ -11,4 +11,38 @@ public class Circle
   private int y;
   private int radio;
   private Color color;
+
+  public Circle(int radio){
+      color = Color.yellow;
+      this.radio = radio;
+    }
+  public void setColor(Color color){
+      this.color = color;
+  }
+  public void setX(int x){
+      this.x = x;
+  }
+  public void setY(int y){
+      this.y = y;
+  }
+  
+  public Color getColor(){
+    return color;
+  }
+  
+  public int getX(){
+      return x;
+  }
+  public int getY(){
+      return y;
+  } 
+  public boolean isInTheCicle(int xPoint, int yPoint){
+      boolean isInTheCicle;
+      if (Math.sqrt(Math.pow(this.x-xPoint,2) + Math.pow(this.y - yPoint,2)) <= radio){
+            isInTheCicle = true; 
+        }else{
+            isInTheCicle = false; 
+        } 
+      return isInTheCicle;
+  }
 }
--- Project2/DrawingPanel.java
+++ Project2/DrawingPanel.java
@@ -3,6 +3,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
+
+
+
 /**
  * Write a description of class DrawingPanel here.
  *
@@ -11,26 +15,35 @@ import java.awt.event.MouseEvent;
  */
 public class DrawingPanel extends JPanel
 {
+     private Random rand = new Random();
+     private Circle circle = new Circle(25);
+
+         
     public DrawingPanel(){
       setBackground(Color.GRAY);
       addMouseListener(new MouseAdapter(){
-        @Override
-        public void mouseClicked(MouseEvent event){
-           System.out.println(String.format("clic at: %s, %s",  event.getX(), event.getY())); 
-        }
+            @Override
+            public void mouseClicked(MouseEvent event){
+               System.out.println(event.getPoint()); 
+               if (circle.isInTheCicle(event.getX(), event.getY())) {
+                   circle.setColor(getRandomColor());
+                }
+               repaint();
+            }
         });
     }
     
     @Override
     public void paint(Graphics g){
         super.paint(g);
-        g.setColor(Color.yellow);
-        
-        int xCenter = getWidth()/2;
-        int yCenter = getHeight()/2;
-        
-        
-        
-        g.fillOval(xCenter - (50/2),yCenter - (50/2),50,50);
+        g.setColor(circle.getColor());
+        circle.setX(getWidth()/2);
+        circle.setY(getHeight()/2);
+        g.fillOval(circle.getX() - (50/2),circle.getY() - (50/2),50,50);
+    }
+    
+    private Color getRandomColor() {
+        return new Color(rand.nextFloat(), rand.nextFloat(),
+        rand.nextFloat());
     }
 }
